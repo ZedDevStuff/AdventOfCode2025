f258cf7e867c3a8c2d4eba6c911321136c1302f1
ZedDevStuff <kouamejuniorauguste@gmail.com>
2025-12-07 11:43:32 +0000

    Day 7 part 1

---
+++ Solutions/Day7.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AdventOfCode2025.Solutions.Misc;
+
+using Spectre.Console;
+
+namespace AdventOfCode2025.Solutions;
+
+public class Day7 : IDay
+{
+    public int Day { get; } = 7;
+    public string Name { get; } = "Laboratories";
+    public string Part1Template { get; } = "[green]Answer:[/] The total amount of time the beam was split is {0}.";
+    public string Part2Template { get; } = "[green]Answer:[/] The total amount of timelines is {0}.";
+    
+    public object ParsePart1(string input)
+    {
+        string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        Grid<char> grid = new Grid<char>(lines[0].Length, lines.Length);
+        for (int y = 0; y < lines.Length; y++)
+            grid.SetRow(y, lines[y].ToCharArray());
+        return grid;
+    }
+
+    public object ParsePart2(string input) => ParsePart1(input);
+
+    public object SolvePart1(object input)
+    {
+        Grid<char> grid = (Grid<char>)input;
+        long total = 0;
+        bool done = false;
+        int startX = grid.IndexOfInRow(0, 'S');
+        List<int> currentPos = [startX];
+        List<int> splits = [];
+        for (int y = 1; y < grid.Height && !done; y++)
+        {
+            List<int> nextPos = [];
+            foreach (var x in currentPos)
+            {
+                if (grid[x, y] == '.')
+                {
+#if DEBUG
+                    grid[x, y] = '|';
+#endif
+                    nextPos.Add(x);
+                }
+                else if (grid[x, y] == '^')
+                {
+                    nextPos.Add(x - 1);
+                    nextPos.Add(x + 1);
+                    total++;
+                }
+            }
+            currentPos = nextPos.Distinct().ToList();
+        }
+#if DEBUG
+        grid.PrintGridCompact(c => c);
+#endif
+        splits = splits.Distinct().ToList();
+        return total;
+    }
+    // Not working yet.
+    public object SolvePart2(object input)
+    {
+        Grid<char> grid = (Grid<char>)input;
+        bool done = false;
+        int startX = grid.IndexOfInRow(0, 'S');
+        int rowsOfSplitters = grid.AsRows().Count(r => r.Contains('^'));
+        char[] directions = new char[rowsOfSplitters];
+        Array.Fill(directions, 'L');
+        List<int> currentPos = [startX];
+        long timelines = 0;
+        while (!done)
+        {
+            timelines++;
+            int splitterIndex = 0;
+            for (int y = 1; y < grid.Height && !done; y++)
+            {
+                List<int> nextPos = [];
+                foreach (var x in currentPos)
+                {
+                    if (grid[x, y] == '.')
+                    {
+#if DEBUG
+                        grid[x, y] = '|';
+#endif
+                        nextPos.Add(x);
+                    }
+                    else if (grid[x, y] == '^')
+                    {
+                        if (directions[splitterIndex] == 'L')
+                            nextPos.Add(x - 1);
+                        else
+                            nextPos.Add(x + 1);
+                        splitterIndex++;
+                    }
+                }
+                currentPos = nextPos.Distinct().ToList();
+            }
+#if DEBUG
+            grid.PrintGridCompact(c => c);
+#endif
+            if (directions.All(d => d == 'R'))
+            {
+                done = true;
+            }
+            else
+            {
+                for (int i = directions.Length - 1; i >= 0; i--)
+                {
+                    if (directions[i] == 'L')
+                    {
+                        directions[i] = 'R';
+                        break;
+                    }
+                    else
+                    {
+                        directions[i] = 'L';
+                    }
+                }
+            }
+        }
+        return timelines;
+    }
+}
---
+++ Solutions/Misc/Grid.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace AdventOfCode2025.Solutions.Misc;
+
+internal class Grid<T>
+{
+    private T[] _backingArray;
+    public readonly int Width;
+    public readonly int Height;
+
+    public Grid(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        _backingArray = new T[width * height];
+    }
+
+    public int IndexOf(T value)
+    {
+        return _backingArray.IndexOf(value);
+    }
+    public int IndexOfInRow(int row, T value)
+    {
+        for (int x = 0; x < Width; x++)
+        {
+            if (this[x, row]?.Equals(value) ?? false)
+                return x;
+        }
+        return -1;
+    }
+    public long IndexOfInColumn(int column, T value)
+    {
+        for (int y = 0; y < Height; y++)
+        {
+            if (this[column, y]?.Equals(value) ?? false)
+                return y;
+        }
+        return -1;
+    }
+
+    public T[] GetRow(int y)
+    {
+        T[] row = new T[Width];
+        Array.Copy(_backingArray, y * Width, row, 0, Width);
+        return row;
+    }
+    public Memory<T> GetRowMemory(int y)
+    {
+        return new Memory<T>(_backingArray, y * Width, Width);
+    }
+    public void SetRow(int y, T[] row)
+    {
+        if (row.Length != Width)
+            throw new ArgumentException("Row length does not match grid width.", nameof(row));
+        Array.Copy(row, 0, _backingArray, y * Width, Width);
+    }
+    public T[] GetColumn(int x)
+    {
+        T[] column = new T[Height];
+        for (int y = 0; y < Height; y++)
+        {
+            column[y] = _backingArray[y * Width + x];
+        }
+        return column;
+    }
+    public Memory<T> GetColumnMemory(int x)
+    {
+        return new Memory<T>(GetColumn(x));
+    }
+    public void SetColumn(int x, T[] column)
+    {
+        if (column.Length != Height)
+            throw new ArgumentException("Column length does not match grid height.", nameof(column));
+        for (int y = 0; y < Height; y++)
+        {
+            _backingArray[y * Width + x] = column[y];
+        }
+    }
+
+    public T[][] AsRows()
+    {
+        T[][] rows = new T[Height][];
+        for (int y = 0; y < Height; y++)
+        {
+            rows[y] = GetRow(y);
+        }
+        return rows;
+    }
+    public T[][] AsColumns()
+    {
+        T[][] columns = new T[Width][];
+        for (int x = 0; x < Width; x++)
+        {
+            columns[x] = GetColumn(x);
+        }
+        return columns;
+    }
+
+    public T this[int x, int y]
+    {
+        get => _backingArray[y * Width + x];
+        set => _backingArray[y * Width + x] = value;
+    }
+
+    public void PrintGridCompact(Func<T, char> toChar)
+    {
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                Console.Write(toChar(this[x, y]));
+            }
+            Console.WriteLine();
+        }
+    }
+}
